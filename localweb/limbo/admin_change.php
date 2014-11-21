fedff14d609cf0696061a3884b3b98269260ad46
DanJast <Daniel.Jast1@marist.edu>
2014-11-20 22:48:01 -0500

    Changes to helpers and added admin_change (unfinished)

---
+++ localweb/limbo/admin_change.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+ <title>Limbo Database</title>
+ <body>
+  <div>
+   <p>
+    <a href="limbo/lost.php">Lost Something</a> <a href="limbo/found.php">Found Something</a> <a href="limbo/quicklinks.php">Quick Links</a>
+   </p>
+  </div>
+	<h1>Welcome to Limbo!</h1>
+	<p>If you lost or found something, you're in luck this is the place to report it.</p>
+	
+	<p>Reported in Last: </p>
+	<form action="">
+		<select name="Options">
+			<option value="week">Week</option>
+			<option value="month">Month</option>
+			<option value="year">Year</option>
+		</select>
+	</form>
+<?php
+# Connect to MySQL server and the database
+require( 'limboincludes/connect_limbo_db.php' ) ;
+
+# Includes these helper functions
+require( 'limboincludes/limbo_helpers.php' ) ;
+
+
+# Show the records
+admin_change_item($dbc);
+
+# Close the connection
+mysqli_close( $dbc ) ;
+?>
+
+ </body>
+</html>
--- localweb/limbo/limboincludes/limbo_helpers.php
+++ localweb/limbo/limboincludes/limbo_helpers.php
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 $debug = true;
 
@@ -175,4 +179,89 @@ else
 # Close the connection
 mysqli_close( $dbc ) ;
 }
+
+function admin_change_item($dbc){
+# Connect to MySQL server and the database
+require( 'connect_limbo_db.php' ) ;
+
+# Create a query to get the name and price sorted by price
+$query = 'SELECT DISTINCT locations.id, stuff_id, name, description, stuff.create_date, stuff.update_date, room, owner, finder, status
+		  FROM locations, stuff
+		  WHERE locations.id = stuff.location_id
+		  ORDER BY locations.id ASC';
+$queryStatus = 'SELECT status FROM stuff ';
+# Execute the query
+$results = mysqli_query( $dbc , $query ) ;
+$resultsStatus = mysqli_query( $dbc , $queryStatus ) ;
+
+
+# Show results
+if( $results AND $resultsStatus ) 
+{
+  # But...wait until we know the query succeeded before
+  # starting the table.
+echo '<H1>Limbo</H1>' ;
+  echo '<TABLE border="1">';
+  echo '<TR>';
+  echo '<TH>Location ID</TH>';
+  echo '<TH>Location</TH>';
+  echo '<TH>Item Description</TH>';
+  echo '<TH>Create Date</TH>';
+  echo '<TH>Update Date</TH>';
+  echo '<TH>Room</TH>';
+  echo '<TH>Owner</TH>';
+  echo '<TH>Finder</TH>';
+  echo '<TH>Status</TH>'; 
+  echo '<TH>Change Status</TH>';
+  echo '</TR>' ;
+  
+while ( $row = mysqli_fetch_array( $resultsStatus , MYSQLI_ASSOC ) )
+  {
+  $changeStatus = 	$row= 	'<select id="cmbChangeStatus" name="ChangeStatus" >
+								<option value="status" >' . $row['status'] . '</option>
+								<option value="Lost" id="lost" >Lost</option>
+								<option value="found" id="found" >Found</option>
+								<option value="status" id="claimed" >Claimed</option>
+							</select>' ;
+	}
+
+  # For each row result, generate a table row
+  while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  {
+
+	echo '<TR>' ;
+    echo '<TD>' . $row['id'] . '</TD>' ;
+	echo '<TD>' . $row['name'] . '</TD>' ;
+	echo '<TD>' . $row['description'] . '</TD>' ;
+    echo '<TD>' . $row['create_date'] . '</TD>' ;
+	echo '<TD>' . $row['update_date'] . '</TD>' ;
+	echo '<TD>' . $row['room'] . '</TD>' ;
+	echo '<TD>' . $row['owner'] . '</TD>' ;
+	echo '<TD>' . $row['finder'] . '</TD>' ;
+	echo '<TD>' . $row['status'] . '</TD>' ;
+	echo '<TD>' . $changeStatus . '</TD>';
+    echo '</TR>' ;
+  }
+  
+  
+  
+  # End the table
+  echo '</TABLE>';
+
+  # Free up the results in memory
+  mysqli_free_result( $results ) ;
+}
+else
+{
+  # If we get here, something has gone wrong
+  echo '<p>' . mysqli_error( $dbc ) . '</p>'  ;
+}
+
+# Close the connection
+mysqli_close( $dbc ) ;
+}
+
 ?>
+
+</body>
+</html>
