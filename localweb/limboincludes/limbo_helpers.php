7ad6442f9f754b7b3682a029066dfb734edc3119
DanJast <Daniel.Jast1@marist.edu>
2014-12-05 12:51:19 -0500

    delete helpers

--- localweb/limboincludes/limbo_helpers.php
+++ localweb/limboincludes/limbo_helpers.php
@@ -238,7 +238,7 @@ else
 # Close the connection
 mysqli_close( $dbc ) ;
 }
-
+##################################################################################################
 function admin_change_item_table($dbc){
 # Connect to MySQL server and the database
 require( '../limboincludes/connect_limbo_db.php' ) ;
@@ -300,6 +300,82 @@ if( $results)
 							</select>
 							</form>
 	</TD>';
+    echo '</TR>' ;
+  }
+  
+  
+  
+  # End the table
+  echo '</TABLE>';
+
+  # Free up the results in memory
+  mysqli_free_result( $results ) ;
+}
+else
+{
+  # If we get here, something has gone wrong
+  echo '<p>' . mysqli_error( $dbc ) . '</p>'  ;
+}
+
+# Close the connection
+mysqli_close( $dbc ) ;
+}
+############################################################################################################################################
+function admin_delete_item($dbc){
+# Connect to MySQL server and the database
+require( '../limboincludes/connect_limbo_db.php' ) ;
+
+# Create a query to get the name and price sorted by price
+$query = 'SELECT DISTINCT locations.id, stuff_id, name, description, stuff.create_date, stuff.update_date, room, owner, finder, status
+		  FROM locations, stuff
+		  WHERE locations.id = stuff.location_id
+		  ORDER BY stuff_id ASC';
+$queryStatus = 'SELECT status FROM stuff ';
+# Execute the query
+$results = mysqli_query( $dbc , $query ) ;
+$resultsStatus = mysqli_query( $dbc , $query ) ;
+
+
+# Show results
+if( $results) 
+{
+  # But...wait until we know the query succeeded before
+  # starting the table.
+  echo '<TABLE border="1">';
+  echo '<TR>';
+  echo '<TH>Item ID</TH>';
+  echo '<TH>Item Description</TH>';
+  echo '<TH>Location ID</TH>';
+  echo '<TH>Location</TH>';
+  echo '<TH>Create Date</TH>';
+  echo '<TH>Update Date</TH>';
+  echo '<TH>Room</TH>';
+  echo '<TH>Owner</TH>';
+  echo '<TH>Finder</TH>';
+  echo '<TH>Status</TH>'; 
+  echo '<TH>Delete Item</TH>';
+  echo '</TR>' ;
+  
+  # For each row result, generate a table row
+  while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  {
+
+	echo '<TR>' ;
+	echo '<TD>' . $row['stuff_id'] . '</TD>' ;
+	echo '<TD>' . $row['description'] . '</TD>' ;
+	echo '<TD>' . $row['id'] . '</TD>' ;
+	echo '<TD>' . $row['name'] . '</TD>' ;
+    echo '<TD>' . $row['create_date'] . '</TD>' ;
+	echo '<TD>' . $row['update_date'] . '</TD>' ;
+	echo '<TD>' . $row['room'] . '</TD>' ;
+	echo '<TD>' . $row['owner'] . '</TD>' ;
+	echo '<TD>' . $row['finder'] . '</TD>' ;
+	echo '<TD>' . $row['status'] . '</TD>' ;
+	echo '<TD> 	<form action="" method="post"> 
+					<input type="hidden" name="id" value=' . $row['stuff_id'] .' />
+					<input type="submit" value="Delete">	
+				</form>
+		</TD>';
     echo '</TR>' ;
   }
   
@@ -470,6 +546,14 @@ $update_query = 'UPDATE stuff SET status="'. $status .'" WHERE stuff_id = '. $id
   check_results($results) ;
 
   return $results ;
+  }
+###########################################################################################################################################
+function delete_item($dbc, $id){
+$delete_query = 'DELETE FROM stuff WHERE stuff_id = '. $id .'';
 
+  $results = mysqli_query($dbc, $delete_query) ;
+  check_results($results) ;
+
+  return $results ;
 }
 ?>
